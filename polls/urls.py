6ad4571014e96afe15011f144b15eb4f49215ce0
Franco <fnc314@gmail.com>
2014-04-19 02:45:45 -0700

    reached part 4 of django tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
+  url(r'^$', views.IndexView.as_view(), name="index"),
   url(r'^$', views.index, name='index'),
   url(r'^(?P<poll_id>\d+)/$', views.detail, name="detail"),
   url(r'^(?P<poll_id>\d+)/results/$', views.results, name="results"),
